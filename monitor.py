33c62be6299781c793e05ed109064f41cc8f66f2
sandrich <mail@chrissandrini.ch>
2021-08-18 20:37:30 +0200

    Add cpu, memory, disk information with verbose output

--- monitor.py
+++ monitor.py
@@ -1,6 +1,7 @@
 import requests
 import json
 import argparse
+import psutil
 from datetime import datetime
 
 def rpcquery(method, rpchost, rpcuser, rpcpassword, params = False):
@@ -55,14 +56,33 @@ def main():
     parser.add_argument('--rpcuser', help='RPC username')
     parser.add_argument('--rpcpassword', help='RPC password')
     parser.add_argument('--rpchost', help='RPC host (default: http://localhost:8554)', default='http://localhost:8554')
+    parser.add_argument('--verbose', action='store_true', help='Prints stats to stdout')
+    parser.add_argument('--defi-path', help='Path to your .defi folder. Example: /home/defi/.defi')
 
     args = parser.parse_args()
 
     if args.rpcuser == None or args.rpcpassword == None:
         exit('Please specify rpcuser and rpcpassword argument')
     
+    if args.defi_path == None:
+        exit('Please specify defi-path argument')
+    
     checkNodes = checkAreNodesMining(args.max_block_seconds, args.rpchost, args.rpcuser, args.rpcpassword)
     print(checkNodes)
+
+    loadavg = psutil.getloadavg()[1]
+    vmem = psutil.virtual_memory()
+    memUsed = vmem.used / 1024**3
+    memTotal = vmem.total / 1024**3
+
+    disk = psutil.disk_usage(args.defi_path)
+    diskUsed = disk.used / 1024**3
+    diskTotal = disk.total / 1024**3
+
+    if args.verbose:
+        print('############ mn server analysis ############')
+        print('Load Average: {:.2f}\nMemory Total: {:.0f} GB\nMemory Used: {:.0f} GB\nDisk Total: {:.0f} GB\nDisk Used: {:.0f} GB'.format(loadavg, memTotal, memUsed, diskTotal, diskUsed))
+        print('############ mn server analysis ############')
     
 
 
