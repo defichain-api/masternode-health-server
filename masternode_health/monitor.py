be0143a410d91234aa401e2bd58880c18de7f513
Chris Sandrini <mail@chrissandrini.ch>
2021-08-25 21:28:51 +0200

    Add more tests (#7)

--- masternode_health/monitor.py
+++ masternode_health/monitor.py
@@ -2,6 +2,7 @@ import requests
 import json
 import argparse
 import psutil
+import sys
 from datetime import datetime, timedelta
 
 
@@ -121,9 +122,9 @@ def processServerStats(args):
     return server_stats
 
 
-def main():
+def parse_args(args):
     parser = argparse.ArgumentParser(description='DefiChain Masternode Monitor')
-    parser.add_argument('--max-block-seconds', help='Alert if node did not try to calculate hash within max-block-seconds (default: 30 seconds)', default=30)
+    parser.add_argument('--max-block-seconds', help='Alert if node did not try to calculate hash within max-block-seconds (default: 30 seconds)', default=30, type=int)
     parser.add_argument('--rpcuser', help='RPC username')
     parser.add_argument('--rpcpassword', help='RPC password')
     parser.add_argument('--rpchost', help='RPC host (default: http://localhost:8554)', default='http://localhost:8554')
@@ -131,16 +132,22 @@ def main():
     parser.add_argument('--defi-path', help='Path to your .defi folder. Example: /home/defi/.defi')
     parser.add_argument('--api-key', help='API Key')
 
-    args = parser.parse_args()
+    args = parser.parse_args(args)
 
     if args.rpcuser is None or args.rpcpassword is None:
-        exit('Please specify rpcuser and rpcpassword argument')
+        raise SystemExit('Please specify rpcuser and rpcpassword argument', )
 
     if args.defi_path is None:
-        exit('Please specify defi-path argument')
+        raise SystemExit('Please specify defi-path argument')
 
     if args.api_key is None:
-        exit('Please specify an api-key argument')
+        raise SystemExit('Please specify an api-key argument')
+
+    return args
+
+
+def main():
+    args = parse_args(sys.argv[1:])
 
     server_stats = processServerStats(args)
     server_info = processNodeInfo(args)
