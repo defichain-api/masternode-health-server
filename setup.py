a9e0fb5133e3e3f89712d7c9efcb6783745edd9d
Chris Sandrini <mail@chrissandrini.ch>
2021-08-25 16:25:31 +0200

    Fix setup script (#5)
    * Add pullrequest trigger for GitHub actions
    
    * Fix setup script
    
    * Fix missing comma

--- setup.py
+++ setup.py
@@ -1,5 +1,10 @@
 from setuptools import setup, find_packages
 
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name='masternode_health',
     version='0.1.0',
@@ -9,7 +14,8 @@ setup(
     url='http://pypi.python.org/pypi/masternode_health',
     license='LICENSE.md',
     description='DeFiChain masternode monitoring solution for DFI Signal Bot',
-    long_description=open('README.md').read(),
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     install_requires=['psutil'],
     entry_points={
         'console_scripts': ['masternode-health=masternode_health.monitor:main']
