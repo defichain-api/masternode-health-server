9d49527390ca7e854e273d5c2d26baf6cf99e758
sandrich <mail@chrissandrini.ch>
2021-08-25 18:54:57 +0200

    Bump version to 0.1.1

--- setup.py
+++ setup.py
@@ -7,13 +7,13 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='masternode_health',
-    version='0.1.0',
+    version='0.1.1',
     author='Christian Sandrini',
     author_email='mail@chrissandrini.ch',
     packages=find_packages(include=['masternode_health', 'masternode_health.*']),
     url='http://pypi.python.org/pypi/masternode_health',
     license='LICENSE.md',
-    description='DeFiChain masternode monitoring solution for DFI Signal Bot',
+    description='This script is designed for collecting server & DeFiChain node information of your system and send them to the DeFiChain Masternode Health API',
     long_description=long_description,
     long_description_content_type='text/markdown',
     install_requires=['psutil'],
